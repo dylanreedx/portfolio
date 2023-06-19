2c114c97f208f86508aaeffc86abdac4132badfa
Dylan <dylan.reeder5@gmail.com>
2023-06-19 11:31:55 -0400

    update to light theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'dark-100': '#737591',
         'dark-200': '#4B4E79',
         'dark-300': '#212235',
         'dark-400': '#191A29',
-        'dark-500': '#141521',
+        'dark-500': '#070707',
+        'light-100': '#D9DBE9',
+        'light-200': '#BEBFD6',
+        'light-300': '#A4A3C3',
         'light-400': '#AAACCA',
         'light-500': '#fff',
       },
